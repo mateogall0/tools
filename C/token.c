a0e9c4238a72e335b788885c17b71552d430792f
mateogall0 <mateogesede@gmail.com>
2024-11-28 09:31:42 -0300

    Add memory protections to str_to_grid

--- C/token.c
+++ C/token.c
@@ -15,9 +15,23 @@ char **str_to_grid(const char *s, const char *delim)
 	for (i = 0; token; i++)
 	{
 		temp[i] = strdup(token);
+        if (!temp[i])
+        {
+            for (j = 0; j < i - 1; j++)
+                free(temp[j]);
+            free(dup);
+            return (NULL);
+        }
 		token = strtok(NULL, delim);
 	}
+    free(dup);
 	grid = malloc(sizeof(char *) * i);
+    if (!grid)
+    {
+        for (j = 0; j < i; j++)
+            free(temp[j]);
+        return (NULL);
+    }
     for (j = 0; j < i; j++)
     {
         grid[j] = temp[j];
