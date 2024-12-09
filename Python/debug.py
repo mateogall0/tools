c536476e9ca0ceed67fb5f6d5da4212741e305e9
Mateo Gallo <92031297+mateogall0@users.noreply.github.com>
2024-12-09 10:29:09 -0300

    Add print_debug

--- Python/debug.py
+++ Python/debug.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+from inspect import currentframe, getfile
 
 DEBUG = True  # in a real scenario this should be a global debug variable
 def timer(func):
@@ -34,14 +35,18 @@ def __example_func():
     90**2
     time.sleep(0.1)
 
-def print_debug(key: str, value: object, separator='=') -> None:
+def print_debug(key='Print debug', value=None, separator='=') -> None:
     """
     Print big values of types like dictionaries with keys tp ease debugging
     process.
     """
     if not DEBUG: return
-    print(key)
-    print(separator * len(key))
+    frame = currentframe().f_back
+    file_name = getfile(frame)
+    line_number = frame.f_lineno
+    text = f'{key} at {file_name}:{line_number}'
+    print(text)
+    print(separator * len(text))
     print(value, end='\n\n')
 
 
